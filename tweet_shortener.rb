d46ede14ffa4f71270f1424b3ebe2267cd84a8ed
Luke Granger <grangerl330@gmail.com>
2019-01-07 23:34:16 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,4 +1,13 @@
 def dictionary
   dictionary = {
-    "hello" => "hi", "to" => "2", "two" => "2", ""
+    "hello" => "hi", 
+    "to" => "2", 
+    "two" => "2", 
+    "too" => "2",
+    "for" => "4",
+    "four" => "4",
+    "be" => "b"
+    "you" => "u"
+    "at" => "@"
+    "and" => "&"
   }
